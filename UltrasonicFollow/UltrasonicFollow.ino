8ccbdf32b5903292c78b0d3cab73f91025f6a741
Noah <noahprince8@gmail.com>
2013-11-21 17:58:30 -0600

    most recent changes

--- UltrasonicFollow/UltrasonicFollow.ino
+++ UltrasonicFollow/UltrasonicFollow.ino
@@ -4,17 +4,30 @@
 Servo myservo;
 //int photoPin = A0;
 int offset = 1;
-int minDistance = 20;
+int minDistance = 50;
   //Ultrasonic ultrasonic(12,13);
 
 void setup(){
 	Serial.begin(9600);
 	myservo.attach(11);
-	pinMode(1,INPUT);
+	pinMode(A1,INPUT);
 }
 void loop() {
+  int distance = 0;
+  for(int i = 15;i<160;i++){
+    myservo.write(i); 
+    int newDistance = 0;
+     for(int i = 0; i<10;i++){
+	 newDistance += analogRead(A1);
+          delay(1);
+     }
+      distance+= newDistance = (int) newDistance/10;
+  }
+  minDistance = (int) (distance/145)/2;
+  Serial.print("MINDISTANCE =");
+  Serial.println(minDistance);
 	//Serial.print(ultrasonic.Ranging(CM));
-  follow(21,minDistance,1,0,0); 
+  follow(21,minDistance,3,0,0,2); 
 }
 
 /*GENERAL IDEA */
@@ -30,59 +43,81 @@ the object again. */
 the direction to move the sensor in (-1 or 1), an int 'verifyFound' to describe whether the 
 function is currently tracking an object and account for sensor error in finding an object; 
 and verifyEdge, an integer used to count how many times the sensor has seen an edge in a row*/
-void follow(int angle, int distance, int dir,int verifyFound,int verifyEdge){
- delay(8);
+void follow(int angle, int distance, int dir,int verifyFound,int verifyEdge,int delayValue){
+ boolean direction = dir > 0; 
    
  angle = angle + dir;
  myservo.write(angle);
- 
+ int newDistance = 0;
+ for(int i = 0; i<14;i++){
+	 newDistance += analogRead(A1);
+          delay(delayValue);
+ }
+ newDistance = (int) newDistance/14;
+
  //If the servo hits the edge of the viewing angle, it's likely lost the object. Start over looking in the opposite directon.
  if(angle > 160){ 
-   follow(159,minDistance,-dir,0,verifyEdge); 
+   follow(159,minDistance,-dir,0,0,delayValue); 
  }
  if(angle < 15){
-   follow(16,minDistance,-dir,0,verifyEdge);
+   follow(16,minDistance,-dir,0,0,delayValue);
  }
  
  //grab sensor data
- int newDistance = analogRead(1);
+
  //int newDistance = ultrasonic.Ranging(CM);
   
  //If we've found an object, follow it until we hit an edge, then reverse direction
- if(verifyFound >= 3){ 
-	 Serial.print(angle);
+ if(verifyFound >= 3){   
+    	 Serial.print(angle);
 	 Serial.print(",");
-	 Serial.print(distance);
-	 Serial.println();
+	 Serial.print(newDistance);
+	 Serial.print("; ");
+         if(direction) dir = 2;
+         else dir = -2; 
+         delayValue = 2;
 
 	 /* verifyFound gets set to 0 when we find an edge. This is because we need to find
 	    the object again when sweeping in the reverse direction, in case it has moved. verifyEdge will
 	    increase every time there's a disparity between the previous distance and the newDistance. 
 	    After verifyEdge is greater than a constant, we can be sure we've hit an edge and not a sensor glitch */ 
-	 if(newDistance >  1.3*distance) verifyEdge++; 
+	 if(newDistance >  distance+10) {
+                Serial.print("    EDGE   (");
+                Serial.print(newDistance);
+                Serial.print(","); 
+                Serial.print(1.3*distance);
+                Serial.print(")           ");   
+                verifyEdge++; 
+         }
 	 else{
 		 if (verifyEdge>0) verifyEdge--; //start removing verifyEdge points if there aren't disparities. 
-                 distance = newDistance;
+                 distance = newDistance+10;
 	 }
    
 	 //We're sure there's an edge, reverse sensor direction and look for the object again
 	 if(verifyEdge>=3){
-		 follow(angle,1.05*distance,-dir,0,0);
+                   Serial.println();
+		 follow(angle,distance+15,-dir,--verifyFound,0,delayValue);
 	 }
    
 	 //Keep following the object
 	 //if(newDistance <= distance) follow(angle,newDistance,dir,true,0); 
-	 follow(angle,distance,dir,verifyFound,verifyEdge);
- }  
+	 follow(angle,distance,dir,verifyFound,verifyEdge,delayValue);
+ }
+ if(verifyFound==0){
+         if(direction) dir = 4;
+         else dir = -4;
+         delayValue = 2; 
+ } 
  
   //When the current proximity is closer than the given distance threshold, we've found an object.
  //Set verifyFound up by one. After a few recursive calls, if this is true for consecutive angles, 
  //we will begin tracking the object. 
  if(newDistance > distance && verifyFound > 0) verifyFound--;
  else if(newDistance <= distance) verifyFound++; 
- else if(verifyFound >= 3) distance = newDistance; 
+ else if(verifyFound >= 3) distance = newDistance+10; 
  
  //catchall. If The object isn't found, just keep scanning
- follow(angle,distance,dir,verifyFound,0);
+ follow(angle,distance,dir,verifyFound,0,delayValue);
 
 }
